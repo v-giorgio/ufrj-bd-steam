6a8305811fed4185348771a0738102e6c9b37907
beatriz almeida ramos <beatriz.ramos1@ibm.com>
2024-11-29 01:21:21 -0300

    added endpoint to get achievements by game and user

---
+++ steamapi/api/steam/src/main/java/com/ufrj/bd/steam/adapters/input/controller/AchievementController.java
@@ -0,0 +1,42 @@
+package com.ufrj.bd.steam.adapters.input.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ufrj.bd.steam.adapters.input.dto.AchievementObtainedDTO;
+import com.ufrj.bd.steam.application.ports.input.GetAchievementByUserAndGameService;
+
+@RestController
+@RequestMapping("/achievement")
+public class AchievementController {
+    private final GetAchievementByUserAndGameService getAchievementByUserAndGameService;
+
+    @Autowired
+    public AchievementController(GetAchievementByUserAndGameService getAchievementByUserAndGameService) {
+        this.getAchievementByUserAndGameService = getAchievementByUserAndGameService;
+    }
+
+    @GetMapping("/user/{userId}/game/{gameId}")
+    public ResponseEntity<List<AchievementObtainedDTO>> getByUserAndGame(@PathVariable Long userId, @PathVariable Long gameId) {
+        System.out.println("entrou");
+
+        try {
+            return ResponseEntity.ok(getAchievementByUserAndGameService.execute(userId, gameId));
+        } catch (RuntimeException e) {
+            System.out.println(e.getMessage());
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @GetMapping
+    public String test() {
+        System.out.println("test");
+        return "test";
+    }
+}
---
+++ steamapi/api/steam/src/main/java/com/ufrj/bd/steam/adapters/output/persistence/repository/AchievementRepository.java
@@ -0,0 +1,17 @@
+package com.ufrj.bd.steam.adapters.output.persistence.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import com.ufrj.bd.steam.adapters.output.persistence.entities.AchievementEntity;
+import com.ufrj.bd.steam.adapters.output.persistence.projections.AchievementObtainedProjection;
+
+public interface AchievementRepository extends JpaRepository<AchievementEntity, String>{
+    
+    @Query("SELECT DISTINCT A.id as name, " +
+    "CASE WHEN UA.achievement.id IS NULL THEN 'false' ELSE 'true' END AS obtained " +
+    "FROM GameUserEntity GU JOIN AchievementEntity A ON A.game.id = GU.game.id LEFT JOIN UserAchievementEntity UA ON UA.achievement.id = A.id WHERE GU.user.id = :userId AND GU.game.id = :gameId")
+    List<AchievementObtainedProjection> getAchivementObtainedByUserAndGame(Long userId, Long gameId);
+}
