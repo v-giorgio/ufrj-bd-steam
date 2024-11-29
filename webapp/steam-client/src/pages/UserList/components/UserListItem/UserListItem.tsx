13940f43eb5668cd203032144a412ed1831552c4
artur_amaral <artur.amaral@mettainnovations.com.br>
2024-11-29 11:25:44 -0300

    Adiciona lista de usuários

---
+++ webapp/steam-client/src/pages/UserList/components/UserListItem/UserListItem.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import styles from './UserListItem.module.css';
+
+import brazilFlag from '../../../../assets/brazil.png'
+
+const UserListItem: React.FC = () => {
+  return (
+    <div className={styles.mainContainer}>
+      <img className={styles.avatar} src="https://avatars.steamstatic.com/9308db1f667688237ceabacc69cde296c28a6533.jpg" />
+      <div className={styles.infoContainer}>
+        <div className={styles.userName}>Juninho666</div>
+        <div className={styles.userCountry}>
+          <img src={brazilFlag} alt="" />
+            BR
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default UserListItem;
