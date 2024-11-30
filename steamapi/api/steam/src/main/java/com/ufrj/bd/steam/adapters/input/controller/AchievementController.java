298ee2fff794e8e08b71ec3cf63a3bab19252aba
v-giorgio <vitorlcardoso17@gmail.com>
2024-11-29 21:40:46 -0300

    feat: swagger

--- steamapi/api/steam/src/main/java/com/ufrj/bd/steam/adapters/input/controller/AchievementController.java
+++ steamapi/api/steam/src/main/java/com/ufrj/bd/steam/adapters/input/controller/AchievementController.java
@@ -2,6 +2,12 @@ package com.ufrj.bd.steam.adapters.input.controller;
 
 import java.util.List;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -23,7 +29,19 @@ public class AchievementController {
     }
 
     @GetMapping("/user/{userId}/game/{gameId}")
-    public ResponseEntity<List<AchievementObtainedDTO>> getByUserAndGame(@PathVariable Long userId, @PathVariable Long gameId) {
+    @Operation(
+            summary = "Obter as conquistas de um usuário para um jogo",
+            description = "Traz todas as conquistas de um usuário para um jogo, tanto as obtidas quanto as não obtidas"
+    )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Conquistas obtidas com sucesso",
+                    content = @Content(mediaType = "application/json", schema = @Schema(implementation = AchievementObtainedDTO.class))),
+            @ApiResponse(responseCode = "500", description = "Erro interno no servidor")
+    })
+    public ResponseEntity<List<AchievementObtainedDTO>> getByUserAndGame(
+            @Parameter(description = "ID do usuário", required = true) @PathVariable Long userId,
+            @Parameter(description = "ID do jogo", required = true) @PathVariable Long gameId
+    ) {
         try {
             return ResponseEntity.ok(getAchievementByUserAndGameService.execute(userId, gameId));
         } catch (RuntimeException e) {
--- steamapi/api/steam/src/main/java/com/ufrj/bd/steam/adapters/input/controller/GameController.java
+++ steamapi/api/steam/src/main/java/com/ufrj/bd/steam/adapters/input/controller/GameController.java
@@ -2,6 +2,11 @@ package com.ufrj.bd.steam.adapters.input.controller;
 
 import com.ufrj.bd.steam.adapters.input.dto.GamesListDTO;
 import com.ufrj.bd.steam.application.ports.input.GetAllGamesService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,6 +25,15 @@ public class GameController {
     }
 
     @GetMapping
+    @Operation(
+            summary = "Obter lista de todos os jogos",
+            description = "Recupera uma lista de todos os jogos disponíveis no sistema."
+    )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Jogos recuperados com sucesso",
+                    content = @Content(mediaType = "application/json", schema = @Schema(implementation = GamesListDTO.class))),
+            @ApiResponse(responseCode = "500", description = "Erro interno no servidor")
+    })
     public ResponseEntity<GamesListDTO> getAll() {
         try {
             return ResponseEntity.ok(getAllGamesService.execute());
